6ffefdf4adc485a8f82eeade697d00e49b7957b2
Rifqi Agnia Mubarok <95258391+rifqiagniamubarok@users.noreply.github.com>
2023-04-13 10:34:48 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
+import { useRef, useState } from 'react';
+
 export default function Home() {
+  const divRef = useRef(null);
+  const [lightMode, setLightMode] = useState(true);
+
+  const handleLighMode = () => {
+    if (lightMode && divRef) {
+      divRef?.current?.classList.add('dark');
+      setLightMode(false);
+    } else {
+      divRef?.current?.classList.remove('dark');
+      setLightMode(true);
+    }
+  };
   return (
-    <html className="">
+    <div className="" ref={divRef}>
       <div className="min-h-screen min-w-screen dark:bg-slate-950 dark:text-white bg-white text-slate-950">
         <p className="text-3xl font-semibold">Hello</p>
         <div className="m-5">
@@ -8,11 +22,14 @@ export default function Home() {
           <p className="text-[48px] font-semibold bg-clip-text text-transparent bg-gradient-to-r from-hexfrom to-hexto">
             I am a Software Engineer
           </p>
-          <button className="bg-gradient-to-r from-hexfrom to-hexto rounded-md px-10 py-3 text-white text-xs hover:opacity-90 w-[246px]">
+          <button
+            className="bg-gradient-to-r from-hexfrom to-hexto rounded-md px-10 py-3 text-white text-xs hover:opacity-90 w-[246px]"
+            onClick={handleLighMode}
+          >
             touch me
           </button>
         </div>
       </div>
-    </html>
+    </div>
   );
 }
